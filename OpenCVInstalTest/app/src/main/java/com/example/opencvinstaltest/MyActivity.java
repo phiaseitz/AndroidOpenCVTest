51b11944e0542410273f1d57db44d8dac0ae23ee
phiaseitz <sophia.seitz@students.olin.edu>
2014-07-18 14:17:20 -0400

    Started the android code

---
+++ OpenCVInstalTest/app/src/main/java/com/example/opencvinstaltest/MyActivity.java
@@ -0,0 +1,10 @@
+package com.example.opencvinstaltest;
+import android.app.Activity;
+
+import org.opencv.android.CameraBridgeViewBase;
+
+
+public class MyActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener{
+
+
+}
